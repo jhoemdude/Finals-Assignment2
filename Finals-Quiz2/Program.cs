3a43032ede190245f1407a42417bf6caa288ebf1
Princess Joem Perida <princessjoem.perida@jmc.edu.ph>
2016-03-21 11:25:42 +0800

    Quiz2

---
+++ Finals-Quiz2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finals_Quiz2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Princess Joem Perida";
+            foreach (char c in s)
+            {
+                Console.WriteLine(System.Convert.ToInt32(c));
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
